8dde59b17bf66385a9963e27a0884cd19614ca12
samimaat <saminiksi@gmail.com>
2021-11-11 19:36:16 +0200

    Store element dictionaries in a separate file

--- SimLab.py
+++ SimLab.py
@@ -1,25 +1,10 @@
+from elements import *
+
 # SimLab game
 
 thing_to_investigate = "A THING"
 
-# Element dictionaries
 
-unknownium = {
-    "Name": "unknownium",
-    "Phase at STP": "Liquid",
-    "Melting point": 0,
-    "Boiling point": 100,
-    "Taste": "Tasteless",
-    "Smell": "Odorless",
-    "Transparency": "Transparent" # What if I want to change transparency at different temperatures?
-
-
-
-}
-
-
-# The element dictionaries as a dictiornaries
-dict_of_elements = {"unknownium": unknownium}
 
 # Functions
 def continue_with_instrument():
---
+++ elements.py
@@ -0,0 +1,15 @@
+# Element dictionaries
+
+unknownium = {
+    "Name": "unknownium",
+    "Phase at STP": "Liquid",
+    "Melting point": 0,
+    "Boiling point": 100,
+    "Taste": "Tasteless",
+    "Smell": "Odorless",
+    "Transparency": "Transparent" # What if I want to change transparency at different temperatures?
+}
+
+
+# The element dictionaries as a dictiornaries
+dict_of_elements = {"unknownium": unknownium}
