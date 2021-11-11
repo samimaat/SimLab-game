ca4a1265635b2e0515f4a7a08c29b3d32a1cbc70
samimaat <saminiksi@gmail.com>
2021-11-11 16:49:34 +0200

    Initial commit

---
+++ SimLab.py
@@ -0,0 +1,8 @@
+# SimLab game
+
+# Choose an element or substance to investigate.
+
+element = input("Choose an element to investigate:" )
+print("You have chosen " + element + " to investigate. Good luck.")
+
+# Choose an instrument to use.
