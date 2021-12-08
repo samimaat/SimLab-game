a429eb65f622681105d4119456c8447ba8be95e0
samimaat <saminiksi@gmail.com>
2021-12-08 20:32:28 +0200

    Change key lookup from dictionaries

--- functions.py
+++ functions.py
@@ -3,8 +3,8 @@ from elements import *
 # Universal Functions
 def choose_element():
     # Add a loop for not choosing an element to study. i.e. There is no such element!
-    element_name = input("Choose an element to investigate: ")
-    return dict_of_elements.get(element_name)
+        element_name = input("Choose an element to investigate: ")
+        return dict_of_elements[element_name]
 
 def continue_with_instrument():
     ans = input("Do you wish to continue with the same instrument? Answer yes [y] or no [n]: ")
@@ -13,7 +13,7 @@ def continue_with_instrument():
 
 # Main Instrument Functions
 def lightbox(element):
-    element_name = element.get("Name")
+    element_name = element["Name"]
 
     print(element_name.capitalize() + f" is now placed in a box. You can see inside only when you have defined a color of the light inside.\nYou can do so by changing the color of the light inside the box.")
 
@@ -28,8 +28,8 @@ def lightbox(element):
 def appearance(color, element):
     # Add functionality to have combined colors. Should be complicated enough.
     ## For example what would I have to do to make the object look white? Or to have black as an option?
-    element_name = element.get("Name")
-    element_color = element.get("Color")
+    element_name = element["Name"]
+    element_color = element["Color"]
 
     if color.lower() == element_color.lower():
         appearance = color
@@ -40,7 +40,7 @@ def appearance(color, element):
 
 
 def heatbox(element):
-    element_name = element.get("Name")
+    element_name = element["Name"]
 
     print(element_name.capitalize() + f" is now placed in a box. You can see inside and you can change the temperature of the box.\nDetermine the melting point and the boiling point of {element_name}.\nYou can do so by changing the temperature inside the box.")
 
@@ -53,8 +53,8 @@ def heatbox(element):
         
 # Subfunction for Heatbox
 def phase(temp, element):
-    element_mp = element.get("Melting point")
-    element_bp = element.get("Boiling point")
+    element_bp = element["Boiling point"]
+    element_mp = element["Melting point"]
 
     # Include funny text for below the absolute zero point!
     if temp <= element_mp:
